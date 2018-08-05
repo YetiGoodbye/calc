45d6fa08881ad83206d65c425589ee58811098df
YetiGoodbye <tar.vxf@gmail.com>
2018-08-05 09:20:38 +0500

    downcase constant SYMBOLS => symbols

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,4 +1,4 @@
-const SYMBOLS = {
+const symbols = {
   D0: '0', D1: '1', D2: '2', D3: '3', D4: '4',
   D5: '5', D6: '6', D7: '7', D8: '8', D9: '9',
   POINT: '.', SIGN: '\u00b1',
@@ -10,6 +10,6 @@ const SYMBOLS = {
 const MAX_DISPLAY_DIGITS = 10;
 
 export {
-  SYMBOLS,
+  symbols,
   MAX_DISPLAY_DIGITS,
 };
