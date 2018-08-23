5d4a764b6ffe8779b1550d8524c873e1d16a598f
YetiGoodbye <tar.vxf@gmail.com>
2018-08-23 15:59:18 +0500

    add index to utils

---
+++ app/utils/index.js
@@ -0,0 +1,12 @@
+import nop from './nop';
+import bemHelper from './bemHelper';
+import createExportableConstants from './createExportableConstants';
+import shallowCopy from './shallowCopy';
+
+export {
+  nop,
+  bemHelper,
+  createExportableConstants,
+  shallowCopy,
+};
+
